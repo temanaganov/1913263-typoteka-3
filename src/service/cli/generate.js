ab9fa6548fb0df3935e0eafced0f0773bc4d28cb
Artem Naganov <artemnaganov10@gmail.com>
2021-10-07 20:16:37 +0300

    Fix

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -3,54 +3,56 @@
 const chalk = require(`chalk`);
 const {getRandomInt, shuffle, getRandomDate, writeJSONFile, readFile} = require(`../../utils`);
 
+const DEFAULT_COUNT = 1;
+const MAX_COUNT = 1000;
+const COUNT = parseInt(process.argv[3], 10) || DEFAULT_COUNT;
+
+const getTitle = (titles) => {
+  return titles[getRandomInt(0, titles.length - 1)];
+};
+
+const getCreatedDate = () => {
+  const currentDate = new Date();
+  const minDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 3, currentDate.getDate());
+
+  return getRandomDate(minDate).toISOString();
+};
+
+const getAnnounce = (announces) => {
+  return shuffle(announces).slice(0, getRandomInt(1, 5)).join(` `);
+};
+
+const getFullText = (announces) => {
+  return shuffle(announces)
+    .slice(0, getRandomInt(1, announces.length - 1))
+    .join(` `);
+};
+
+const getCategory = (categories) => {
+  return shuffle(categories).slice(0, getRandomInt(1, categories.length - 1));
+};
+
 const generate = async () => {
   try {
-    const TITLES = await readFile(`data/titles.txt`);
-    const ANNOUNCES = await readFile(`data/sentences.txt`);
-    const CATEGORIES = await readFile(`data/categories.txt`);
-    const DEFAULT_COUNT = 1;
-    const MAX_COUNT = 1000;
-
-    const count = parseInt(process.argv[3], 10) || DEFAULT_COUNT;
-
-    const getTitle = () => {
-      return TITLES[getRandomInt(0, TITLES.length - 1)];
-    };
-
-    const getCreatedDate = () => {
-      const currentDate = new Date();
-      const minDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 3, currentDate.getDate());
-
-      return getRandomDate(minDate).toISOString();
-    };
-
-    const getAnnounce = () => {
-      return shuffle(ANNOUNCES).slice(0, getRandomInt(1, 5)).join(` `);
-    };
-
-    const getFullText = () => {
-      return shuffle(ANNOUNCES)
-        .slice(0, getRandomInt(1, ANNOUNCES.length - 1))
-        .join(` `);
-    };
-
-    const getCategory = () => {
-      return shuffle(CATEGORIES).slice(0, getRandomInt(1, CATEGORIES.length - 1));
-    };
-
-    if (count > MAX_COUNT) {
+    if (COUNT > MAX_COUNT) {
       console.error(chalk.red(`Не больше 1000 объявлений`));
       process.exit(1);
     }
 
-    const result = Array(count)
+    const [TITLES, ANNOUNCES, CATEGORIES] = await Promise.all([
+      readFile(`data/titles.txt`),
+      readFile(`data/sentences.txt`),
+      readFile(`data/categories.txt`),
+    ]);
+
+    const result = Array(COUNT)
       .fill({})
       .map(() => ({
-        title: getTitle(),
+        title: getTitle(TITLES),
         createdDate: getCreatedDate(),
-        announce: getAnnounce(),
-        fullText: getFullText(),
-        category: getCategory(),
+        announce: getAnnounce(ANNOUNCES),
+        fullText: getFullText(ANNOUNCES),
+        category: getCategory(CATEGORIES),
       }));
 
     await writeJSONFile(`mocks.json`, result);
