6e62668258bf47734447d23faced0cc9580be919
Artem Naganov <artemnaganov10@gmail.com>
2021-10-07 20:10:33 +0300

    WIP

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -3,46 +3,50 @@
 const chalk = require(`chalk`);
 const http = require(`http`);
 const status = require(`../../status-codes`);
-const {readFile} = require(`../../utils`);
+const data = require(`../../../mocks.json`);
 
 const startServer = async () => {
-  const TITLES = await readFile(`data/titles.txt`);
-  const DEFAULT_PORT = 3000;
-  const port = parseInt(process.argv[3], 10) || DEFAULT_PORT;
+  try {
+    const DEFAULT_PORT = 3000;
+    const port = parseInt(process.argv[3], 10) || DEFAULT_PORT;
+    const titles = data.map((item) => item.title);
 
-  const responseText = `
-<!DOCTYPE html>
-  <html lang="ru">
-  <head>
-    <title>Titles</title>
-  </head>
-  <body>
-    <ul>
-      ${TITLES.map((title) => `<li>${title}</li>`)}
-    </ul>
-  </body>
-</html>`;
+    const listItems = titles.map((title) => `<li>${title}</li>`).join(``);
 
-  const listener = (req, res) => {
-    console.log(`fsdf`);
-    res.writeHead(status.HTTP_SUCCESS_CODE, {
-      'Content-Type': `text/html; charset=UTF-8`,
+    const responseText = `
+    <!DOCTYPE html>
+      <html lang="ru">
+      <head>
+        <title>Titles</title>
+      </head>
+      <body>
+        <ul>${listItems}</ul>
+      </body>
+    </html>`;
+
+    const listener = (req, res) => {
+      res.writeHead(status.HTTP_SUCCESS_CODE, {
+        'Content-Type': `text/html; charset=UTF-8`,
+      });
+
+      if (req.url === `/`) {
+        res.end(responseText);
+      }
+    };
+
+    const httpServer = http.createServer(listener);
+
+    httpServer.listen(port, () => {
+      console.info(chalk.green(`Server started at port ${port}`));
     });
 
-    if (req.url === `/`) {
-      res.end(responseText);
-    }
-  };
-
-  const httpServer = http.createServer(listener);
-
-  httpServer.listen(port, () => {
-    console.info(chalk.green(`Server started at port ${port}`));
-  });
-
-  httpServer.on(`error`, ({message}) => {
-    console.error(chalk.red(`Ошибка: ${message}`));
-  });
+    httpServer.on(`error`, ({message}) => {
+      console.error(chalk.red(`Ошибка: ${message}`));
+    });
+  } catch (error) {
+    console.error(chalk.red(error));
+    process.exit(1);
+  }
 };
 
 module.exports = {
