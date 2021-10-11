ac8caecb3cfc0423fd2392fdfffd9e125d832fe2
Artem Naganov <artemnaganov10@gmail.com>
2021-10-11 20:18:13 +0300

    WIP

--- src/express/express.js
+++ src/express/express.js
@@ -2,6 +2,7 @@
 
 const express = require(`express`);
 const chalk = require(`chalk`);
+const path = require(`path`);
 const articlesRouter = require(`./routes/articles.routes`);
 const myRouter = require(`./routes/my.routes`);
 const mainRouter = require(`./routes/main.routes`);
@@ -9,6 +10,11 @@ const mainRouter = require(`./routes/main.routes`);
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+app.locals.basedir = app.get(`views`);
+
+app.use(express.static(path.resolve(__dirname, `public`)));
 app.use(`/articles`, articlesRouter);
 app.use(`/my`, myRouter);
 app.use(`/`, mainRouter);
