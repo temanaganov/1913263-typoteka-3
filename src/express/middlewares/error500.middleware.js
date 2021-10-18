4737a03c6fe2cf629cb8d47bbd07bcf941b5de33
Artem Naganov <artemnaganov10@gmail.com>
2021-10-18 21:17:22 +0300

    Fix

--- src/express/middlewares/error500.middleware.js
+++ src/express/middlewares/error500.middleware.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
-const errorMiddleware = (err, req, res) => {
+const errorMiddleware = (err, req, res, next) => {
   console.error(err.stack);
   res.status(500).render(`500`);
 };
